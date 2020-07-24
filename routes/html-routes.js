381d7273c60fb413af602c87c851c65608738fa0
jarrellrn <63278388+jarrellrn@users.noreply.github.com>
2020-07-23 19:25:18 -0500

    added css link

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,14 @@
 const path = require("path");
 var exphbs = require("express-handlebars");
+const express = require("express");
 
 module.exports = function (app) {
   app.engine("handlebars", exphbs({ defaultLayout: "main" }));
   app.set("view engine", "handlebars");
+
+  //   app.use(
+  //     express.static(path.join(__dirname, "./public/assets/css/style.css"))
+  //   );
 
   app.get("/", function (req, res) {
     res.render("index");
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -7,6 +8,7 @@ var db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, "./public/assets/css/style.css")));
 
 app.use(express.static("public"));
 
