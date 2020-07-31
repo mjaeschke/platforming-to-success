d101a12b83a7da40678739f1d45c8bf5f5ca455b
Jesus Garza <jesuscgarza33@gmail.com>
2020-07-31 18:31:21 -0500

    bear

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,146 +1,319 @@
-// The attributes of the player.
-var player = {
-  x: 200,
+(function () {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+var canvas = document.getElementById("canvas"),
+  ctx = canvas.getContext("2d"),
+  width = 1000,
+  height = 400,
+  player = {
+      x: 200,
+      y: 200,
+      width: 25,
+      height: 25,
+      speed: 3,
+      velX: 0,
+      velY: 0,
+      jumping: false,
+      grounded: false,
+      color:'#E6AC27'
+  },
+  keys = [],
+  friction = 0.8,
+  gravity = 0.4,
+  boxes = [],
+  powerup = [];  
+
+powerup.push({
+      x: 810,
+      y: 250,
+      width: 20,
+      height: 20,
+      color: '#BF4D28',
+      effect: 'shrink'
+  });
+powerup.push({
+      x: 400,
+      y: 150,
+      width: 20,
+      height: 20,
+      color: '#BF4D28',
+      effect: 'gravity'
+  });
+powerup.push({
+      x: -15,
+      y: 88,
+      width: 20,
+      height: 20,
+      color: '#222',
+      effect: 'tele',
+      rotate: 20,
+      px: 20,//where they get teleported
+      py: 370,
+      stay: true
+  });
+powerup.push({
+      x: 60,
+      y: 365,
+      width: 20,
+      height: 20,
+      color: '#2A5D77',
+      effect: 'win',
+      stay: true
+  });
+
+// dimensions
+boxes.push({//box on left
+  x: 0,
+  y: height/4+10,
+  width: 10,
+  height: height,
+  color: 'green'
+});
+boxes.push({//box on left
+  x: 0,
+  y: 0,
+  width: 10,
+  height: height/4-15,
+  color: 'green'
+});
+boxes.push({//box for the ground
+  x: 0,
+  y: height - 10,
+  width: width,
+  height: 50,
+  color: 'orange'
+});
+boxes.push({//box on right
+  x: width - 10,
+  y: 0,
+  width: 50,
+  height: height,
+  color: 'yellow'
+});
+boxes.push({
+  x: 290,
   y: 200,
-  x_v: 0,
-  y_v: 0,
-  jump: true,
-  height: 30,
-  width: 20,
-};
-// The status of the arrow keys
-var keys = {
-  right: false,
-  left: false,
-  up: false,
-};
-// The friction and gravity to show realistic movements
-var gravity = 0.8;
-var friction = 0.7;
-// The number of platforms
-var num = 2;
-// The platforms
-var platforms = [];
-// Function to render the canvas
-function rendercanvas() {
-  ctx.fillStyle = "#F0F8FF";
-  ctx.fillRect(0, 0, 800, 1000);
-}
-// Function to render the player
-function renderplayer() {
-  ctx.fillStyle = "#F08080";
-  ctx.fillRect(player.x - 20, player.y - 20, player.width, player.height);
-}
-// Function to create platforms
-function createplat() {
-  for (i = 0; i < num; i++) {
-    platforms.push({
-      x: 100 * i,
-      y: 200 + 30 * i,
-      width: 110,
-      height: 15,
-    });
+  width: 260,
+  height: 10,
+  color: 'blue'
+});
+boxes.push({
+  x: 590,
+  y: 200,
+  width: 80,
+  height: 10,
+  color: 'blue'
+});
+boxes.push({
+  x: 120,
+  y: 250,
+  width: 150,
+  height: 10,
+  color: 'red'
+});
+boxes.push({
+  x: 220,
+  y: 300,
+  width: 80,
+  height: 10,
+  color: 'black'
+});
+boxes.push({
+  x: 340,
+  y: 350,
+  width: 90,
+  height: 10,
+  color: '#655643'
+});
+boxes.push({
+  x: 740,
+  y: 300,
+  width: 160,
+  height: 10,
+  color: '#655643'
+});
+boxes.push({
+  x: 0,
+  y: 350,
+  width: 90,
+  height: 10,
+  color: '#655643'
+});
+boxes.push({
+  x: 90,
+  y: 350,
+  width: 10,
+  height: 50,
+  color: '#655643'
+});
+//boxes.push({
+  //x: 600,
+  //y: 200,
+  //width: 60,
+  //height: 10,
+  //color: '#655643'
+//});
+
+canvas.width = width;
+canvas.height = height;
+
+function update() {
+  // check keys
+  if (keys[38] || keys[32] || keys[87]) {
+      // up arrow or space
+      if (!player.jumping && player.grounded) {
+          player.jumping = true;
+          player.grounded = false;
+          player.velY = -player.speed * 2.5;//how high to jump
+      }
   }
-}
-// Function to render platforms
-function renderplat() {
-  ctx.fillStyle = "#45597E";
-  ctx.fillRect(
-    platforms[0].x,
-    platforms[0].y,
-    platforms[0].width,
-    platforms[0].height
-  );
-  ctx.fillRect(
-    platforms[1].x,
-    platforms[1].y,
-    platforms[1].width,
-    platforms[1].height
-  );
-}
-// This function will be called when a key on the keyboard is pressed
-function keydown(e) {
-  // 37 is the code for the left arrow key
-  if (e.keyCode == 37 || e.keyCode == 65) {
-    keys.left = true;
+  if (keys[39] || keys[68]) {
+      // right arrow
+      if (player.velX < player.speed) {
+          player.velX++;
+      }
   }
-  // 37 is the code for the up arrow key
-  if (e.keyCode == 38 || e.keyCode == 32 || e.keyCode == 87) {
-    if (player.jump == false) {
-      player.y_v = -10;
+  if (keys[37] || keys[65]) {
+      // left arrow
+      if (player.velX > -player.speed) {
+          player.velX--;
+      }
+  }
+
+  
+
+  player.velX *= friction;
+  player.velY += gravity;
+
+  ctx.clearRect(0, 0, width, height);
+  ctx.beginPath();
+  
+  player.grounded = false;
+  for (var i = 0; i < boxes.length; i++) {//print boxes
+      ctx.fillStyle = boxes[i].color;
+      ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+      
+      var dir = colCheck(player, boxes[i]);
+
+      if (dir === "l" || dir === "r") {
+          player.velX = 0;
+          player.jumping = false;
+      } else if (dir === "b") {
+          player.grounded = true;
+          player.jumping = false;
+      } else if (dir === "t") {
+          player.velY *= -1;
+      }
+
+  }
+  
+  if(player.grounded){
+       player.velY = 0;
+  }
+  
+  player.x += player.velX;
+  player.y += player.velY;
+
+  ctx.fill();//Draw charater stuff
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+  
+  //draw powerup stuff 
+  for(var j = 0; j < powerup.length; j++){
+    ctx.save();
+    var cx = powerup[j].x + 0.5 * powerup[j].width,   // x of shape center
+    cy = powerup[j].y + 0.5 * powerup[j].height; //y of shape center
+    ctx.translate(cx, cy);  //translate to center of shape
+    ctx.rotate( (Math.PI / 180) * 45);//rotate 25 degrees.
+    if(powerup[j].effect  === 'tele'){
+      ctx.rotate( (Math.PI / 180) * powerup[j].rotate);//rotate 25 degrees.
+      powerup[j].rotate = (Math.PI / 180) * powerup[j].rotate;
+    }
+    ctx.translate(-cx, -cy);            //translate center back to 0,0
+    ctx.fillStyle = powerup[j].color;
+    ctx.fillRect(powerup[j].x, powerup[j].y, powerup[j].width, powerup[j].height);
+    ctx.restore();
+    
+    //powerup collision
+    if(colCheck(player, powerup[j])!==null){//touched power up!
+      if(powerup[j].effect==='gravity'){
+        gravity= 0.4;//decrease gravity
+        player.speed = 4;
+        player.color = 'white';
+      }
+      else if (powerup[j].effect==='shrink'){
+        player.width= 10;
+        player.height= 10;
+        player.speed = 5;
+      }
+      else if (powerup[j].effect==='tele'){
+        player.x=powerup[j].px;
+        player.y=powerup[j].py;
+      }
+      else if (powerup[j].effect==='win'){
+        var r = confirm("You win! Play again?");
+        if (r == false) {
+             player.x=200;
+             player.y=200;
+        } else {
+             window.location.href = window.location.href;
+        }
+      }
+      if(powerup[j].stay!==true)
+      powerup[j].width=0;//make power up go away
     }
   }
-  // 39 is the code for the right arrow key
-  if (e.keyCode == 39 || e.keyCode == 68) {
-    keys.right = true;
-  }
+  //powerup stuff
+
+  requestAnimationFrame(update);
 }
-// This function is called when the pressed key is released
-function keyup(e) {
-  if (e.keyCode == 37 || e.keyCode == 65) {
-    keys.left = false;
+
+function colCheck(shapeA, shapeB) {
+  // get the vectors to check against
+  var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+      vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+      // add the half widths and half heights of the objects
+      hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+      hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+      colDir = null;
+
+  // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
+  if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+      // figures out on which side we are colliding (top, bottom, left, or right)
+      var oX = hWidths - Math.abs(vX),
+          oY = hHeights - Math.abs(vY);
+      if (oX >= oY) {
+          if (vY > 0) {
+              colDir = "t";
+              shapeA.y += oY;
+          } else {
+              colDir = "b";
+              shapeA.y -= oY;
+          }
+      } else {
+          if (vX > 0) {
+              colDir = "l";
+              shapeA.x += oX;
+          } else {
+              colDir = "r";
+              shapeA.x -= oX;
+          }
+      }
   }
-  if (e.keyCode == 38 || e.keyCode == 32 || e.keyCode == 87) {
-    if (player.y_v < -2) {
-      player.y_v = -3;
-    }
-  }
-  if (e.keyCode == 39 || e.keyCode == 68) {
-    keys.right = false;
-  }
+  return colDir;
 }
-function loop() {
-  // If the player is not jumping apply the effect of frictiom
-  if (player.jump == false) {
-    player.x_v *= friction;
-  } else {
-    // If the player is in the air then apply the effect of gravity
-    player.y_v += gravity;
-  }
-  player.jump = true;
-  // If the left key is pressed increase the relevant horizontal velocity
-  if (keys.left) {
-    player.x_v = -2.5;
-  }
-  if (keys.right) {
-    player.x_v = 2.5;
-  }
-  // Updating the y and x coordinates of the player
-  player.y += player.y_v;
-  player.x += player.x_v;
-  // A simple code that checks for collions with the platform
-  let i = -1;
-  if (
-    platforms[0].x < player.x &&
-    player.x < platforms[0].x + platforms[0].width &&
-    platforms[0].y < player.y &&
-    player.y < platforms[0].y + platforms[0].height
-  ) {
-    i = 0;
-  }
-  if (
-    platforms[1].x < player.x &&
-    player.x < platforms[1].x + platforms[1].width &&
-    platforms[1].y < player.y &&
-    player.y < platforms[1].y + platforms[1].height
-  ) {
-    i = 1;
-  }
-  if (i > -1) {
-    player.jump = false;
-    player.y = platforms[i].y;
-  }
-  // Rendering the canvas, the player and the platforms
-  rendercanvas();
-  renderplayer();
-  renderplat();
-}
-canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-ctx.canvas.height = 800;
-ctx.canvas.width = 600;
-createplat();
-// Adding the event listeners
-document.addEventListener("keydown", keydown);
-document.addEventListener("keyup", keyup);
-setInterval(loop, 22);
+
+document.body.addEventListener("keydown", function (e) {
+  keys[e.keyCode] = true;
+});
+
+document.body.addEventListener("keyup", function (e) {
+  keys[e.keyCode] = false;
+});
+
+
+window.addEventListener("load", function () {
+  update();
+});
