783447ed3fbb84df00a3b67a7634fabc2e146e3a
jarrellrn <63278388+jarrellrn@users.noreply.github.com>
2020-07-31 15:34:10 -0500

    added keyboard functionality

--- public/assets/js/indexphaser.js
+++ public/assets/js/indexphaser.js
@@ -32,6 +32,7 @@ function preload() {
 function create() {
   this.add.image(400, 300, "sky");
 
+  // Start of platforms instance
   platforms = this.physics.add.staticGroup();
 
   platforms.create(400, 568, "ground").setScale(2).refreshBody();
@@ -39,6 +40,55 @@ function create() {
   platforms.create(600, 400, "ground");
   platforms.create(50, 250, "ground");
   platforms.create(750, 220, "ground");
+
+  // Start of player instance
+  player = this.physics.add.sprite(100, 450, "dude");
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+  player.body.setGravityY(300);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  this.physics.add.collider(player, platforms);
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play("right", true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play("turn");
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
